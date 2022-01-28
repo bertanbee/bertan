590e09d9492abc484d88a73975d7b0151fdb5282
Lucas Bertan <bertan1707@gmail.com>
2022-01-28 20:49:53 -0300

    Update Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
-import { FaGithub, FaInstagram, FaDiscord, FaReact, FaPython, FaJs, FaCss3Alt, FaUnity } from 'react-icons/fa';
-import { SiCsharp,SiBlender } from 'react-icons/si'
+import { FaGithub, FaInstagram, FaDiscord, FaReact, FaPython, FaJs, FaNodeJs, FaFigma } from 'react-icons/fa';
+import { SiBlender } from 'react-icons/si'
 
 export default function Card () {
     const CardBox = styled.div`
@@ -220,9 +220,9 @@ export default function Card () {
                         <a rel="noreferrer" className="noStyleAnchor" target="_blank" href="https://reactnative.dev/"><FaReact className="iconbadge"></FaReact></a>
                     </MiniBadgeDiv>
                     <MiniBadgeDiv>
-                        <a rel="noreferrer" className="noStyleAnchor" target="_blank" href="https://docs.microsoft.com/en-us/dotnet/csharp/"><SiCsharp className="iconbadge"></SiCsharp></a>
-                        <a rel="noreferrer" className="noStyleAnchor" target="_blank" href="https://unity.com/"><FaUnity className="iconbadge"></FaUnity></a>
                         <a rel="noreferrer" className="noStyleAnchor" target="_blank" href="https://www.blender.org/"><SiBlender className="iconbadge"></SiBlender></a>
+                        <a rel="noreferrer" className="noStyleAnchor" target="_blank" href="https://nodejs.org/"><FaNodeJs className="iconbadge"></FaNodeJs></a>
+                        <a rel="noreferrer" className="noStyleAnchor" target="_blank" href="https://www.figma.com/"><FaFigma className="iconbadge"></FaFigma></a>
                     </MiniBadgeDiv>
                     
                 </BadgesDiv>
