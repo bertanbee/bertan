7bd909f752d422f5fe7bbdbcfee3c6cade804942
Lucas Bertan <bertan1707@gmail.com>
2022-02-14 13:09:30 -0300

    Update Curriculum.js

--- src/Routes/Curriculum.js
+++ src/Routes/Curriculum.js
@@ -29,6 +29,7 @@ export default function Curriculum () {
         <CurriculumBox>
             <Header></Header>
             <Curriculo>
+                <InfoCardRight ano="2022" mes="FEVEREIRO" premio="Engenharia de Computação-Esc. Politécnica da USP" quem="Fuvest" link1="LISTÃO" link1href="https://www.fuvest.br/wp-content/uploads/fuvest_2022_chamada_1.pdf"></InfoCardRight>
                 <InfoCardLeft ano="2022" mes="JANEIRO" premio="1° EP/7° AC Engenharia Mecânica - UNESP" quem="Vunesp" link1="Listão" link1href="https://documento.vunesp.com.br/documento/stream/MjY1OTU3Ng%3d%3d"></InfoCardLeft>
                 <InfoCardRight ano="2022" mes="JANEIRO" premio="Medalha de Prata-OBMEP" quem="IMPA" link1="PREMIADOS" link1href="http://premiacao.obmep.org.br/16aobmep/verRelatorioPremiadosPrata.do.htm"></InfoCardRight>
                 <InfoCardLeft ano="2022" mes="JANEIRO" premio="Menção Honrosa-OBF" quem="SBF" link1="PREMIADOS" link1href="http://www.sbfisica.org.br/v1/olimpiada/2021/index.php/16-obf/283-vencedores-da-obf-2021-3-serie"></InfoCardLeft>
