7d46fcc0952be9464c66c5aed16b706973fe9cdc
Lucas Bertan <bertan1707@gmail.com>
2022-07-05 18:39:10 -0300

    Update Home.js

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,12 +1,11 @@
 import Header from '../components/Header/Header';
 import Presentation from '../components/Presentation/Presentation';
-
 import NewTopic from "../components/NewTopic/NewTopic";
 import NewTopic1 from "../components/NewTopic1/NewTopic1";
 import NewTopic3 from "../components/NewTopic3/NewTopic3";
 import Footer from "../components/Footer/Footer";
 
-import { useEffect } from 'react'
+import { useEffect } from 'react';
 
 function Home() {
 
